4715f3e6861bd57e5cf13f6c75b19f355eb024ad
Oscar Green <fsug@yahoo.com>
2015-10-11 05:10:45 -0400

    intermediate quiz3

---
+++ prep_files/ruby_workbook/intermediate/quiz3/ex5.rb
@@ -0,0 +1,13 @@
+def color_valid(color)
+	color == "blue" || color == "green" ? true : false
+end
+
+puts color_valid('orange')
+
+#bookway
+
+def color_valid(color)
+	color == "blue" || color == "green"
+end
+
+puts color_valid("blue")
